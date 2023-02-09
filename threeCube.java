f5df71c9d500c1bbbb8a20b64b24f64155d594bf
192011423 <112488194+192011423@users.noreply.github.com>
2023-02-09 22:31:22 +0530

    Add files via upload

---
+++ threeCube.java
@@ -0,0 +1,20 @@
+class threeCube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+		int n = 45;
+		
+		if(n == 0 || n ==1 || n % 3 !=0) {
+			System.out.println("False");
+			}else{
+			if(n % 3 == 0 || isNumber(n)) {
+				System.out.println("True");
+				}
+			}
+		}
+	private static boolean isNumber(int n) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
