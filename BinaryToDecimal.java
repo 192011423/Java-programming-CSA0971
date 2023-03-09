28bb1e22ac70dee5c090e8c880f88ccb05202f75
192011423 <112488194+192011423@users.noreply.github.com>
2023-03-09 21:04:22 +0530

    Add files via upload

---
+++ BinaryToDecimal.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class DecimalToBinary
+{
+public static void main(String[] args)
+{
+System.out.println(Integer.parseInt("2",1010));
+System.out.println(Integer.parseInt("7",111));
+System.out.println(Integer.parseInt("16",11111));
+}
+}
