89eaf0275bdcbc9540446589a58c047404f7ebf7
192011423 <112488194+192011423@users.noreply.github.com>
2023-03-09 21:09:37 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class table
+{
+public static void main(String[] args)
+{
+System.out.println("enter the number:");
+Scanner sc=new Scanner(System.in);
+int m=sc.nextInt();
+int n=sc.nextInt();
+for(int i=1;i<=3;i++)
+{
+System.out.println(3+"*"+i+"="+3*i);}
+for(int j=4;j<=6;j++)
+{
+System.out.println(4+"*"+j+"="+4*j);}
+for(int k=7;k<=10;k++)
+{
+System.out.println(5+"*"+k+"="+5*k);
+}
+}
+}
